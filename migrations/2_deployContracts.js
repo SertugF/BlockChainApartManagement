0880a17d8caf8b12201fa30435a448dcccec63bb
SertugF <sertugfidan@outlook.com>
2022-12-16 23:18:26 +0300

    delete unwanted files

--- migrations/2_deployContracts.js
+++
@@ -1,7 +0,0 @@
-const ToDoList = artifacts.require("ToDoList");
-
-module.exports = function (deployer) {
-  deployer.deploy(ToDoList, "ToDoList");
-};
-
-// block chain is kind a like big database so we enter the data in the form of blocks using migrations.
